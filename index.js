d0f540b12dd7f7b8fecb07b85b0d83a0e885c27f
Amir Mahmoud <nfutoam.atef@gmail.com>
2012-12-30 18:41:36 +0200

    Finish Basbosa-validation modules as same as the previous validation_modules.js Replace validation_modules with require('basbosa-validation') and ensure from all current methods

--- index.js
+++ index.js
@@ -23,9 +23,6 @@
 	BasbosaValidation.prototype.Rules = {
 		urlPattern : new RegExp("((http|https)(:\/\/))?([a-zA-Z0-9]+[.]{1}){2}[a-zA-z0-9]+(\/{1}[a-zA-Z0-9]+)*\/?", "i"),
 		isObjectId : function(obj, field, options, cb) {
-			// Checks if field is an object id
-			// (i.e. a 12 byte hex number, taking into consideration that it's represented in a string)
-			// Check if field is a string (includes checking if it's present)
 			if (typeof options === 'function') {
 		    cb = options;
 		    options = undefined;
@@ -35,28 +32,22 @@
 					if(cb === 'function')	cb(false);
 					return false;
 				}
-				// checking that each character represents a hex numbers using a regular expression
 				if(cb === 'function')	cb(/[\da-f]{24}/.test(obj));
 				return  /[\da-f]{24}/.test(obj);	
 			} else {
-				if (!this.string(obj, field)) {
+				if (!BasbosaValidation.prototype.Rules.string(obj, field)) {
 					if(cb === 'function')	cb(false);
 					return false;
 				}
-				// Check if length is 24 characters (i.e. 12 bytes)
 				if (obj[field].length != 24) {
 					if(cb === 'function')	cb(false);
 					return false;
 				}
-				// checking that each character represents a hex numbers using a regular expression
 				if(cb === 'function')	cb(/[\da-f]{24}/.test(obj[field]));
 				return  /[\da-f]{24}/.test(obj[field]);	
 			}
-			
 		},
 		isPresent: function(obj, field, options, cb) {
-			// Checks if field exists in a obj or not
-			
 			if (typeof options === 'function') {
 		    cb = options;
 		    options = undefined;
@@ -64,10 +55,8 @@
 			if(cb === 'function')	cb(_.has(obj, field));
 			return _.has(obj, field);
 		},
-
 		string: function(obj, field, options, cb) {
-			// Checks if field inside obj is a string or not
-			if (typeof options === 'function') {
+			if(typeof options === 'function') {
 		    cb = options;
 		    options = undefined;
 		  }
@@ -75,14 +64,11 @@
 				if(cb === 'function') cb(_.isString(field));
 				return  _.isString(field);
 			} else {
-				if(cb === 'function')	cb(this.isPresent(obj, field) && _.isString(obj[field]));
-				return this.isPresent(obj, field) && _.isString(obj[field]);
+				if(cb === 'function')	cb(BasbosaValidation.prototype.Rules.isPresent(obj, field) && _.isString(obj[field]));
+				return BasbosaValidation.prototype.Rules.isPresent(obj, field) && _.isString(obj[field]);
 			}
-			
-			
 		},
 		numeric: function(obj, field, options, cb) {
-			// Checks if field inside obj is an integer or not
 			if (typeof options === 'function') {
 		    cb = options;
 		    options = undefined;
@@ -91,14 +77,11 @@
 				if(cb === 'function') cb(_.isNumber(obj));
 				return  _.isNumber(obj);
 			} else {
-				if(cb === 'function')	cb(this.isPresent(obj, field) && _.isNumber(obj[field]));
-				return this.isPresent(obj, field) && _.isNumber(obj[field]);
+				if(cb === 'function')	cb(BasbosaValidation.prototype.Rules.isPresent(obj, field) && _.isNumber(obj[field]));
+				return BasbosaValidation.prototype.Rules.isPresent(obj, field) && _.isNumber(obj[field]);
 			}
-			
 		},
 		url: function(obj, field, options, cb) {
-			var self = this;
-			// Checks if field inside obj is a url or not
 			if (typeof options === 'function') {
 		    cb = options;
 		    options = undefined;
@@ -110,27 +93,22 @@
 			}
 			if(obj ===	undefined) {
 				if(cb === 'function') cb(self.urlPattern.test(field));
-				return (self.urlPattern.test(field));
+				return (BasbosaValidation.prototype.Rules.urlPattern.test(field));
 			} else {
-				if(cb === 'function')	cb(this.isPresent(obj, field) && self.urlPattern.test(obj[field]));
-				return this.isPresent(obj, field) && self.urlPattern.test(obj[field]);
+				if(cb === 'function')	cb(BasbosaValidation.prototype.Rules.isPresent(obj, field) && 
+						BasbosaValidation.prototype.Rules.urlPattern.test(obj[field]));
+				return BasbosaValidation.prototype.Rules.isPresent(obj, field) && 
+						BasbosaValidation.prototype.Rules.urlPattern.test(obj[field]);
 			}
 			
 		},
-		/**
-		 * @method isLargerThan used to check if the length of any string  is larger than specific number.
-		 * @param {String} obj hold the message  that contain specific string that need t check on it.
-		 * @param {String} field hold the string that need to check on it.
-		 * @param {Number} min hold the number that need to limit the length of the string by it.
-		 */
 		maxLength: function(obj, field, options, cb) {
-			// Checks if field inside obj is larger than the min value
 			if (typeof options === 'function') {
 		    cb = options;
 		    options = undefined;
 		  } 
 			if(options === undefined) {
-				options = field ;
+				options = field;
 				field = obj;
 				obj = undefined;
 			}
@@ -138,10 +116,11 @@
 				if(cb === 'function') cb((field.length >= options));
 				return (field.length >= options);
 			} else {
-				if(cb === 'function') cb(this.isPresent(obj, field) && (obj[field].length >= options));
-				return this.isPresent(obj, field) && (obj[field].length >= options);
+				if(cb === 'function') cb(BasbosaValidation.prototype.Rules.isPresent(obj, field) && 
+						(obj[field].length >= options));
+				return BasbosaValidation.prototype.Rules.isPresent(obj, field) && 
+						(obj[field].length >= options);
 			}
-			
 		},
 		email :  function(obj, field, options, cb) {
 			if (typeof field === 'function') {
@@ -153,14 +132,12 @@
 		    cb = options;
 		    options = {};
 		  }
-			
 			if(cb === 'function') {
 				cb(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(obj));
 			}
 			return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(obj);
 		},
 		minLength: function(obj, field, options, cb) {
-			var self = this;
 			if (typeof options === 'function') {
 		    cb = options;
 		    options = undefined;
@@ -174,77 +151,60 @@
 				if(cb === 'function') cb(field.length <= options);
 				return field.length <= options;
 			} else {
-				if(cb === 'function') cb(self.isPresent(obj, field) && (obj[field].length <= options));
-				// Checks if field inside obj is smaller than the max value
-				return self.isPresent(obj, field) && (obj[field].length <= options);
+				if(cb === 'function') cb(BasbosaValidation.prototype.Rules.isPresent(obj, field) && 
+						(obj[field].length <= options));
+				return BasbosaValidation.prototype.Rules.isPresent(obj, field) && 
+						(obj[field].length <= options);
 			}
 		},
 		rang: function(obj, field, options, cb) {
-			// Checks if field inside obj is within the min and max values
 			if(typeof options === 'function') {
 				cb = options;
 				options = field;
 				field = obj;
 				obj = undefined;
 			}
-			if(cb === 'function')	cb(this.minLength(obj, field, options.max) && this.maxLength(obj, field, options.min));
-			return this.minLength(obj, field, options.max) && this.maxLength(obj, field, options.min);
+			if(cb === 'function')	cb(BasbosaValidation.prototype.Rules.minLength(obj, field, options.max) && 
+					BasbosaValidation.prototype.Rules.maxLength(obj, field, options.min));
+			return BasbosaValidation.prototype.Rules.minLength(obj, field, options.max) && 
+					BasbosaValidation.prototype.Rules.maxLength(obj, field, options.min);
 		},
-
-
 	};
 	BasbosaValidation.prototype.validate = function(validator, msg, callback) {
-	//Does the actual validation
 		if(_.isEmpty(validator)) {
-			// We either drop or pass a message with an empty validator here, except what happens
-			// for authn.socket? There is nothing being passed in it
-			// return true; was originally like this
-			
-			typeof callback === 'funcrion' && callback(false);
-			return false; // now any message with empty validator will simply be dropped
-	
-		}	else { // Validate
+			typeof callback === 'function' && callback(false);
+			return false;
+		}	else {
 			var validation_results = new Array();
 			_.each(validator, function(fields) {
 				_.each(fields, function(validators, key) {
 					_.each(validators, function(obj) {
-						// obj could be a function or an array that contains a function + extra paramters to be given
-						// while invoking that function later on in the validation process
-						// so first we check if it's an array
 						if(_.isArray(obj)) {
-							// handle this
 							var func = _.first(obj);
-							var args = _.union( [msg, key], _.rest(obj, 1) ); // We are ignoring the first item in obj array
-							// since this is the validation function name that'll be called below
-							if( func.length < _.size( args ) ) { // Arguments passed in validator must not be more
-								// than expected function arguments
-								typeof callback === 'funcrion' && callback(false);
+							var args = _.union( [msg, key], _.rest(obj, 1) ); 
+							if( func.length < _.size(args) ) {
+								typeof callback === 'function' && callback(false);
 								return false;
-							} else if( func.length > _.size( args ) ) { // or less
-								typeof callback === 'funcrion' && callback(false);
+							} else if( func.length > _.size( args ) ) {
+								typeof callback === 'function' && callback(false);
 								return false;
-							} else { // If we reach this final else, then they are equal
+							} else {
 								validation_results.push( func.apply(null, args ) );
 							}
 						} else {
-							// must have been a function
 							validation_results.push(obj(msg, key));
 						}
 					});
-	
 				});
-	
 			});
-	
 			if (_.all(validation_results, _.identity)) {
-				typeof callback === 'funcrion' && callback(true);
+				typeof callback === 'function' && callback(true);
 				return true;
 			} else {
-				typeof callback === 'funcrion' && callback(false);
+				typeof callback === 'function' && callback(false);
 				return false;
 			}
 		}
-		console.log('hereeeeeeeeeee');
 	};
 	if (typeof instance === 'undefined') instance = new BasbosaValidation;
   if (typeof Basbosa !== 'undefined') Basbosa.add('BasbosaValidation', instance);
